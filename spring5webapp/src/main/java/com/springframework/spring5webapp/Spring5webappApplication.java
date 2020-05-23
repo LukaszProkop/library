2d01062caa9144bb06169f616ab8768c59623467
Lukasz Prokop <Lukasz.Marek.Prokop@gmail.com>
2020-05-23 02:16:00 +0200

    Add CRUD repos and fill up database with sample data

--- spring5webapp/src/main/java/com/springframework/spring5webapp/Spring5webappApplication.java
+++ spring5webapp/src/main/java/com/springframework/spring5webapp/Spring5webappApplication.java
@@ -1,13 +1,18 @@
 package com.springframework.spring5webapp;
 
+import com.springframework.spring5webapp.bootstrap.DevBootstrap;
+import com.springframework.spring5webapp.repositories.AuthorRepository;
+import com.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5webappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5webappApplication.class, args);
+    }
 
 }
